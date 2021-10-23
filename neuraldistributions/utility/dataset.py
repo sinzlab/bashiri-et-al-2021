277631514f4daf6391d23f2e86ae868da5c87627
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2021-10-23 17:04:34 +0200

    Add utilities to read image and adjust the size and entry values to the model

--- neuraldistributions/utility/dataset.py
+++ neuraldistributions/utility/dataset.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 import numpy as np
+from PIL import Image
 import torch
-
 from torch.utils import data
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import Sampler
@@ -58,3 +58,31 @@ def get_dataloader(
     data_loader = data.DataLoader(data_set, batch_size=batch_size, shuffle=shuffle)
 
     return data_loader
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def imread(filename, grayscale=True, xres=None, yres=None):
+
+    img = Image.open(filename)
+    h, w, *_ = np.array(img).shape
+    aspectratio = h / w
+
+    xres = xres if xres is not None else w
+    yres = yres if yres is not None else int(xres * aspectratio)
+
+    image = np.array(img.resize((xres, yres), Image.ANTIALIAS))
+
+    gray_image = (
+        rgb2gray(image)[None, None, :, :].astype(np.float32)
+        if grayscale
+        else image.transpose(2, 0, 1)[None, :, :, :].astype(np.float32)
+    )
+
+    # make sure the entries are between 0 and 1
+    gray_image = gray_image - gray_image.min()
+    gray_image = gray_image / gray_image.max()
+
+    return gray_image
